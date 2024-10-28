3c4757f7e7621b1f05c8185f9645ef6d4d8ec1e0
Yuri Starov <yuri.starov@gmail.com>
2024-10-28 17:46:09 +0100

    HW2 complited

--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -10,22 +10,30 @@ import (
 )
 
 func ReadJSON(filePath string) ([]types.Employee, error) {
+	// Открытие файла
 	f, err := os.Open(filePath)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		// Возвращаем ошибку, если файл не удалось открыть
+		return nil, fmt.Errorf("ошибка при открытии файла: %w", err)
 	}
-
+	// Гарантируем закрытие файла после выполнения функции
+	defer f.Close()
+	// Чтение содержимого файла
 	bytes, err := io.ReadAll(f)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return nil, nil
+		// Возвращаем ошибку, если чтение не удалось
+
+		return nil, fmt.Errorf("ошибка при чтении файла: %w", err)
 	}
 
+	// Распаковка JSON в слайс сотрудников
 	var data []types.Employee
 
 	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		// Возвращаем ошибку, если Unmarshal не удался
+		return nil, fmt.Errorf("ошибка при распаковке JSON: %w", err)
+	}
 
-	res := data
-
-	return res, nil
+	return data, nil
 }
