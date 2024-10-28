49a579d87da2d782e8740c9b38d335898268d627
Yuri Starov <yuri.starov@gmail.com>
2024-10-28 17:31:10 +0100

    HW2 complited

--- hw02_fix_app/go.mod
+++ hw02_fix_app/go.mod
@@ -1,5 +1,3 @@
 module github.com/fixme_my_friend/hw02_fix_app
 
 go 1.20
-
-require github.com/golangci/golangci-lint v1.61.0 // indirect
--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -9,7 +9,7 @@ import (
 func PrintStaff(staff []types.Employee) {
 	var str string
 	for i := 0; i < len(staff); i++ {
-		str := fmt.Sprintf("User ID: %d; Age: %d;", staff[i].UserID, staff[i].Age)
+		str = fmt.Sprintf("User ID: %d; Age: %d;", staff[i].UserID, staff[i].Age)
 		str2 := fmt.Sprintf(`Name: %s; Department ID: %d; `, staff[i].Name, staff[i].DepartmentID)
 		fmt.Println(str, str2)
 	}
