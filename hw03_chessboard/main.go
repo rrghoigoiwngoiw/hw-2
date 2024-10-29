9c5ed04caff2ac75459bdd86e9614895a66bc4fa
Yuri Starov <yuri.starov@gmail.com>
2024-10-29 14:17:22 +0100

    написал код

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var chessSize int
+
+	fmt.Print("Введите размер доски: ")
+	_, err := fmt.Scan(&chessSize)
+	if err != nil || chessSize <= 0 {
+		fmt.Println("Ошибка: введите положительное целое число")
+		return
+	}
+
+	for y := 0; y < chessSize; y++ {
+		for x := 0; x < chessSize; x++ {
+			if (y+x)%2 == 0 {
+				fmt.Print("# ")
+			} else {
+				fmt.Print("  ")
+			}
+		}
+		fmt.Print("\n")
+	}
 }
