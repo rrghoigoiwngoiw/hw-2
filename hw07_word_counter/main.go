9942f1bcccf79faa7188c7806bca0f23babd3068
Yuri Starov <yuri.starov@gmail.com>
2024-11-23 09:47:55 +0100

    hw7 готов1

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"strings"
+	"unicode"
+)
+
+func countWords(text string) map[string]int {
+	text = strings.ToLower(text)
+
+	words := strings.FieldsFunc(text, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+
+	wordCount := make(map[string]int)
+
+	for _, word := range words {
+		wordCount[word]++
+	}
+
+	return wordCount
+}
+
 func main() {
 	// Place your code here.
+	// 1. написать функцию принимающую строку и возвращающую ее буз лишних знаков,
+	// в нижнем регистре
+	// 2. написать мапу попеременно включающую эти слова как ключи
+	// сразу проверять количество этих слов в данном тексте и записывать результат как значение  в мапу
 }
