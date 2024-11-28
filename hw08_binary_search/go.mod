3894b96ca356f567bbd9f442edbcb6f2a25027cc
Yuri Starov <yuri.starov@gmail.com>
2024-11-28 20:41:22 +0100

    HW7 написанно1

--- hw08_binary_search/go.mod
+++ hw08_binary_search/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw08_binary_search
+module github.com/rrghoigoiwngoiw/hw-2/hw08_binary_search
 
 go 1.20
--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,20 @@
-package main
+package binarysearch
 
-func main() {
-	// Place your code here.
+import "sort"
+
+func BinarySearch(input []int, search int) bool {
+	sort.Ints(input)
+	low, high := 0, len(input)-1
+	for low <= high {
+		mid := low + (high-low)/2
+		if input[mid] == search {
+			return true
+		}
+		if input[mid] < search {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return false
 }
