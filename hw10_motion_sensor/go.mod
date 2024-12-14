1e0ad5722f0cbffa4d54c9f49e736962215741ec
Yuri Starov <yuri.starov@gmail.com>
2024-12-14 10:50:19 +0100

    HW10 написан1

--- hw10_motion_sensor/go.mod
+++ hw10_motion_sensor/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw10_motion_sensor
+module github.com/frrghoigoiwngoiw/hw-2/hw10_motion_sensor
 
 go 1.20
--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,5 +1,46 @@
-package main
+package sensor
 
-func main() {
-	// Place your code here.
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Sensor() {
+	sensorData := make(chan int)
+	processedData := make(chan float64)
+
+	go func() {
+		defer close(sensorData) // Закрываем канал после завершения работы
+		start := time.Now()
+		for time.Since(start) < 1*time.Minute {
+			data := rand.Intn(100)
+			sensorData <- data
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// Горутина для обработки данных
+	go func() {
+		defer close(processedData) // Закрываем канал после завершения работы
+		buffer := make([]int, 0, 10)
+		for data := range sensorData {
+			buffer = append(buffer, data)
+			if len(buffer) == 10 {
+				sum := 0
+				for _, value := range buffer {
+					sum += value
+				}
+				average := float64(sum) / 10.0
+				processedData <- average
+				buffer = buffer[:0]
+			}
+		}
+	}()
+
+	for result := range processedData {
+		fmt.Printf("Среднее значение: %.2f\n", result)
+	}
+
+	fmt.Println("Завершение обработки данных.")
 }
