f49146da0bd551afcdb59e43469b3642d29cdc00
Yuri Starov <yuri.starov@gmail.com>
2025-01-27 16:31:45 +0100

    HW13 is completed 1.0

---
+++ hw13_http/client/client_test.go
@@ -0,0 +1,37 @@
+package client
+
+import (
+	"flag"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestRunClient_MissingURL(t *testing.T) {
+	// Командная строка для запуска текущего теста
+	cmd := exec.Command(os.Args[0], "-test.run=TestRunClient_MissingURL_Helper")
+	cmd.Env = append(os.Environ(), "TEST_MISSING_URL=1")
+
+	// ошибкa
+	err := cmd.Run()
+	if exitError, ok := err.(*exec.ExitError); ok {
+		if exitError.ExitCode() != 1 {
+			t.Errorf("ожидался код выхода 1, получен: %d", exitError.ExitCode())
+		}
+	} else if err == nil {
+		t.Error("ожидался код выхода 1, программа завершилась без ошибок")
+	}
+}
+
+func TestRunClient_MissingURL_Helper(t *testing.T) {
+	if os.Getenv("TEST_MISSING_URL") != "1" {
+		return
+	}
+
+	// некорректные флаги
+	os.Args = []string{os.Args[0], "-method=GET"}
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	RunClient()
+	os.Exit(0)
+}
